e48abf872732932988575b9b85865784b41b9801
FaheemAsghar01 <faheemasghar629@gmail.com>
2023-05-15 15:15:44 +0500

    Change token expiration time

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -58,7 +58,7 @@ export class AuthService {
     };
     const secret = this.config.get('JWT_SECRET');
     const token = await this.jwt.signAsync(payload, {
-      expiresIn: '15s',
+      expiresIn: '1h',
       secret: secret,
     });
     return {
